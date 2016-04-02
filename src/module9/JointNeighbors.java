9a45acfab66c0df0bcb9db1d3b8a7de4ffa4be3b
Adeel Qureshi <adeelmahmood@gmail.com>
2016-04-02 18:15:13 -0500

    m9

--- src/module9/JointNeighbors.java
+++ src/module9/JointNeighbors.java
@@ -7,13 +7,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.math3.random.EmpiricalDistribution;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
-import com.panayotis.gnuplot.JavaPlot;
-import com.panayotis.gnuplot.plot.DataSetPlot;
-import com.panayotis.gnuplot.plot.Plot;
 import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
@@ -24,20 +22,40 @@ public class JointNeighbors {
 	private MGraph graph;
 	public static final String BASE_DIRECTORY = "GraphDatabases";
 	private static final String GRAPH_FILE = BASE_DIRECTORY + "/" + graphName + ".graphml";
-	private static final String OUT_GRAPH_FILE = BASE_DIRECTORY + "/" + graphName + ".out.graphml";
+
+	private Map<Double, Double> directProbs = new HashMap<Double, Double>();
+	private Map<Double, Double> nonDirectProbs = new HashMap<Double, Double>();
 
 	public JointNeighbors() {
 		graph = GraphUtils.readGraph(GRAPH_FILE);
 
-//		System.out.println("[\tDirect Joint Neighbors\t]");
-//		List<Double> values1 = countDirectJointNeighbors();
-//		generateHistogram(values1);
+		System.out.println("[\tDirect Joint Neighbors\t]");
+		List<Double> values1 = countDirectJointNeighbors();
+		generateHistogram(values1, directProbs);
 
 		System.out.println("\n[\tNonDirect Joint Neighbors\t]");
 		List<Double> values2 = countNonDirectJointNeighbors();
-		generateHistogram(values2);
+		generateHistogram(values2, nonDirectProbs);
+
+		System.out.println();
+		for (Entry<Double, Double> e : directProbs.entrySet()) {
+			Double num = e.getValue();
+			Double denom = nonDirectProbs.get(e.getKey());
+			System.out.print(e.getKey() + " => " + num + "/" + denom + " = " + num / denom);
+			if (num / denom > 1) {
+				System.out.println(" (link)");
+			} else {
+				System.out.println();
+			}
+		}
 	}
 
+	/**
+	 * Direct joint neighbors are the vertices that are connected to v and w
+	 * vertices when v and w are connected to each other
+	 * 
+	 * @return
+	 */
 	private List<Double> countDirectJointNeighbors() {
 		List<Double> values = new ArrayList<Double>();
 		Map<String, String> processed = new HashMap<String, String>();
@@ -53,12 +71,19 @@ public class JointNeighbors {
 
 				double count = countJointNeighbors(v, w);
 				values.add(count);
-//				System.out.println(v.getId() + ", " + w.getId() + " = " + count);
+				// System.out.println(v.getId() + ", " + w.getId() + " = " +
+				// count);
 			}
 		}
 		return values;
 	}
 
+	/**
+	 * Non direct joint neighbors are the vertices that are connected to v and w
+	 * vertices when v and w are not connected to each other
+	 * 
+	 * @return
+	 */
 	private List<Double> countNonDirectJointNeighbors() {
 		List<Double> values = new ArrayList<Double>();
 		Map<String, String> processed = new HashMap<String, String>();
@@ -77,12 +102,16 @@ public class JointNeighbors {
 
 				double count = countJointNeighbors(v, w);
 				values.add(count);
-//				System.out.println(v.getId() + ", " + w.getId() + " = " + count);
+				// System.out.println(v.getId() + ", " + w.getId() + " = " +
+				// count);
 			}
 		}
 		return values;
 	}
 
+	/**
+	 * Counts the number of joint neighbors for given vertex v and w
+	 */
 	private double countJointNeighbors(Vertex v, Vertex w) {
 		int numOfJointNeighbors = 0;
 		for (Vertex u : graph.getVertices()) {
@@ -104,6 +133,9 @@ public class JointNeighbors {
 		return (double) numOfJointNeighbors;
 	}
 
+	/**
+	 * Determines if given vertex w is connected to vertex v
+	 */
 	private boolean isNeighbor(Vertex v, Vertex w) {
 		for (Edge e : w.getEdges(Direction.OUT)) {
 			if (e.getVertex(Direction.IN) == v) {
@@ -113,7 +145,12 @@ public class JointNeighbors {
 		return false;
 	}
 
-	private void generateHistogram(List<Double> values) {
+	/**
+	 * Given a set of values, this function generates histogram and populates a
+	 * given map of probablities where the keys are number of joint neighbors
+	 * and values are how often
+	 */
+	private void generateHistogram(List<Double> values, Map<Double, Double> probs) {
 		double[] data = new double[values.size()];
 		int i = 0;
 		for (double v : values) {
@@ -123,27 +160,15 @@ public class JointNeighbors {
 		EmpiricalDistribution histogram = new EmpiricalDistribution(data.length);
 		histogram.load(data);
 
-		int count = histogram.getBinCount();
-		// we will be storing the probality for each bin here
-		double[][] probabilities = new double[count][count];
-		int j = 0;
-		
 		List<SummaryStatistics> stats = histogram.getBinStats();
 		System.out.println("N == " + histogram.getSampleStats().getN());
 		for (SummaryStatistics stat : stats) {
 			if (stat.getN() > 0) {
 				System.out.println("Number of joint neighbors=" + stat.getMax() + ", How often=" + stat.getN()
 						+ ", Probablity=" + ((double) stat.getN() / histogram.getSampleStats().getN()));
+				probs.put(stat.getMax(), ((double) stat.getN() / histogram.getSampleStats().getN()));
 			}
-			probabilities[j][0] = stat.getMax(); 
-			probabilities[j++][1] = ((double) stat.getN() / histogram.getSampleStats().getN());
 		}
-		
-		JavaPlot p = new JavaPlot();
-		// pass the probabilities matrix to the plot
-		Plot dat = new DataSetPlot(probabilities);
-		p.addPlot(dat);
-		p.plot();
 	}
 
 	@SuppressWarnings("unused")
